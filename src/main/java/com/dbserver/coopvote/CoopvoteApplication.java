cf267ce3bc7e40929056333a82e0f03b7bbf48b0
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 15:04:22 -0300

    init: inicia projeto

---
+++ src/main/java/com/dbserver/coopvote/CoopvoteApplication.java
@@ -0,0 +1,13 @@
+package com.dbserver.coopvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbserver/coopvote/CoopvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbserver.coopvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
