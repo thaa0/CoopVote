bdf33d218415b95797475ff4642c201cd85b0824
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 19:19:51 -0300

    feat: implementa request e response da api

--- src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/SessaoAberturaResquest.java
+++ src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/SessaoAberturaResquest.java
@@ -1,5 +1,16 @@
 package com.dbserver.coopvote.sessaoVotacao.application.controller;
 
-public class SessaoAberturaResquest {
+import java.util.Optional;
+import java.util.UUID;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class SessaoAberturaResquest {
+	@NotNull
+	private UUID idPauta;
+	private Integer tempoDuracao;
+	
+	Optional<Integer> getTempoDuracao(){return Optional.ofNullable(tempoDuracao);}
 }
--- src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/SessaoVotacaoAPI.java
+++ src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/SessaoVotacaoAPI.java
@@ -1,7 +1,13 @@
 package com.dbserver.coopvote.sessaoVotacao.application.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dbserver.coopvote.sessaoVotacao.application.service.SessaoVotacaoService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -14,7 +20,9 @@ public class SessaoVotacaoAPI {
 	
 	private final SessaoVotacaoService sessaoVotacaoService;
 
-	public SessaoAbertaResponse abreSessao(SessaoAberturaResquest novaSessao) {
+	@PostMapping("/abertura")
+	@ResponseStatus(HttpStatus.CREATED)
+	public SessaoAbertaResponse abreSessao(@RequestBody SessaoAberturaResquest novaSessao) {
 		log.info("[start] SessaoVotacaoAPI - abreSessao");
 		SessaoAbertaResponse sessaoAbertaResponse = sessaoVotacaoService.abreSessao(novaSessao);
 		log.debug("[finish] SessaoVotacaoAPI - abreSessao");
