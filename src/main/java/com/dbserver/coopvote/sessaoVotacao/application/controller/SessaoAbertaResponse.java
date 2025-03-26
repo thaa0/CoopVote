e95da5649939e3ff04f098052500afbaf7f84e09
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 19:20:57 -0300

    feat: implementa response da api

--- src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/SessaoAbertaResponse.java
+++ src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/SessaoAbertaResponse.java
@@ -1,5 +1,21 @@
 package com.dbserver.coopvote.sessaoVotacao.application.controller;
 
-public class SessaoAbertaResponse {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import com.dbserver.coopvote.sessaoVotacao.domain.StatusSessaoVotacao;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+
+@Getter
+public class SessaoAbertaResponse {
+	private UUID id;
+	private UUID idPauta;
+	private Integer tempoDuracao;
+	@Enumerated(EnumType.STRING)
+	private StatusSessaoVotacao status;
+	private LocalDateTime dataHoraAbertura;
+	private LocalDateTime dataHoraEncerramento;
 }
