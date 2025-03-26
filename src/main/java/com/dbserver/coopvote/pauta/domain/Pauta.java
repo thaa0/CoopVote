4048e34c7b6b560d7f4cfc62347060f4238c009c
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 19:59:25 -0300

    refactor: refatora domain Pauta tirando imports desnecessarios

--- src/main/java/com/dbserver/coopvote/pauta/domain/Pauta.java
+++ src/main/java/com/dbserver/coopvote/pauta/domain/Pauta.java
@@ -1,15 +1,18 @@
 package com.dbserver.coopvote.pauta.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.Valid;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import com.dbserver.coopvote.pauta.application.controller.PautaNovaRequest;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
