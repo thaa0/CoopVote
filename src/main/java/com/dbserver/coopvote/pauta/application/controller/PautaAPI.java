9c92a7a3a2b2ee02758ec7ad5d259c0f6381bdda
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 16:44:22 -0300

    feat: implementa fluxo controller

--- src/main/java/com/dbserver/coopvote/pauta/application/controller/PautaAPI.java
+++ src/main/java/com/dbserver/coopvote/pauta/application/controller/PautaAPI.java
@@ -1,13 +1,25 @@
 package com.dbserver.coopvote.pauta.application.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pauta")
 @Log4j2
 @RequiredArgsConstructor
 public class PautaAPI {
+
+    private final PautaService pautaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PautaCriadaResponse cadastraPauta(@RequestBody @Valid PautaNovaRequest pauta) {
+        log.info("[start] PautaAPI - cadastraPauta");
+        PautaCriadaResponse pautaCriada = pautaService.cadastraPauta(pauta);
+        log.debug("[finish] PautaAPI - cadastraPauta");
+        return pautaCriada;
+    }
 }
--- src/main/java/com/dbserver/coopvote/pauta/application/controller/PautaNovaRequest.java
+++ src/main/java/com/dbserver/coopvote/pauta/application/controller/PautaNovaRequest.java
@@ -1,4 +1,14 @@
 package com.dbserver.coopvote.pauta.application.controller;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
 public class PautaNovaRequest {
+    private String titulo;
+    private String descricao;
+    private UUID idAssociadoCriador;
+    private LocalDateTime dataHoraCriacao;
 }
