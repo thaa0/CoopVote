165d988ca729a7ebe0f9cb22c78323f7b6974fe8
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 16:35:21 -0300

    chore: configurar Swagger no projeto - ajustes no application.yml e pom.xml

--- src/main/java/com/dbserver/coopvote/pauta/application/controller/PautaAPI.java
+++ src/main/java/com/dbserver/coopvote/pauta/application/controller/PautaAPI.java
@@ -1,4 +1,13 @@
 package com.dbserver.coopvote.pauta.application.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pauta")
+@Log4j2
+@RequiredArgsConstructor
 public class PautaAPI {
 }
