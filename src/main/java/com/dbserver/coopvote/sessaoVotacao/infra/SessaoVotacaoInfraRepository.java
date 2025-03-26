3323e42c39bdc367a5542d919165d0f385874e9d
Thalita Souza <dev.thalita@gmail.com>
2025-03-26 20:07:30 -0300

    feat: implementa fluxo infra

--- src/main/java/com/dbserver/coopvote/sessaoVotacao/infra/SessaoVotacaoInfraRepository.java
+++ src/main/java/com/dbserver/coopvote/sessaoVotacao/infra/SessaoVotacaoInfraRepository.java
@@ -13,9 +13,12 @@ import lombok.extern.log4j.Log4j2;
 @Repository
 public class SessaoVotacaoInfraRepository implements SessaoVotacaoRepository {
 
+	private final SessaoVotacaoSpringDataJpaRepository sessaoVotacaoSpringDataJpaRepository;
+
 	@Override
 	public void save(SessaoVotacao sessaoCriada) {
 		log.info("[start] SessaoVotacaoInfraRepository - save");
+		sessaoVotacaoSpringDataJpaRepository.save(sessaoCriada);
 		log.debug("[finish] SessaoVotacaoInfraRepository - save");
 	}
 
