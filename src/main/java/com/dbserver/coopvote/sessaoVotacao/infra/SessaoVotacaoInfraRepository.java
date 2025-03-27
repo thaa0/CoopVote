aaf3a0f98331878de619b1c6b39ec2c9fa2de818
Thalita Souza <dev.thalita@gmail.com>
2025-03-27 19:36:41 -0300

    feat: implementa fluxo repository de busca sessao por id

--- src/main/java/com/dbserver/coopvote/sessaoVotacao/infra/SessaoVotacaoInfraRepository.java
+++ src/main/java/com/dbserver/coopvote/sessaoVotacao/infra/SessaoVotacaoInfraRepository.java
@@ -1,5 +1,6 @@
 package com.dbserver.coopvote.sessaoVotacao.infra;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import com.dbserver.coopvote.sessaoVotacao.application.repository.SessaoVotacaoRepository;
@@ -7,6 +8,8 @@ import com.dbserver.coopvote.sessaoVotacao.domain.SessaoVotacao;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Log4j2
@@ -20,6 +23,15 @@ public class SessaoVotacaoInfraRepository implements SessaoVotacaoRepository {
 		log.info("[start] SessaoVotacaoInfraRepository - save");
 		sessaoVotacaoSpringDataJpaRepository.save(sessaoCriada);
 		log.debug("[finish] SessaoVotacaoInfraRepository - save");
+	}
+
+	@Override
+	public SessaoVotacao buscaSessaoPorId(UUID idSessaoVotacao) {
+		log.info("[start] SessaoVotacaoInfraRepository - buscaSessaoPorId");
+		SessaoVotacao sessao = sessaoVotacaoSpringDataJpaRepository.findById(idSessaoVotacao)
+				.orElseThrow(() -> new EntityNotFoundException("Sessao Nao Encontrada"));
+		log.debug("[finish] SessaoVotacaoInfraRepository - buscaSessaoPorId");
+		return sessao;
 	}
 
 }
