f86776baea8ef8dc866f552721db9ef822e4746a
Thalita Souza <dev.thalita@gmail.com>
2025-03-29 17:14:54 -0300

    test: implementa cenarios de cpf valido e invalido ao registrar voto

--- src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/VotoRequest.java
+++ src/main/java/com/dbserver/coopvote/sessaoVotacao/application/controller/VotoRequest.java
@@ -1,11 +1,13 @@
 package com.dbserver.coopvote.sessaoVotacao.application.controller;
 
 import com.dbserver.coopvote.sessaoVotacao.domain.OpcaoVoto;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 public class VotoRequest {
     private String cpfAssociado;
     private OpcaoVoto opcaoVoto;
--- src/test/java/test/java/com/dbserver/coopvote/sessaoVotacao/application/service/SessaoVotacaoAplicationServiceTest.java
+++ src/test/java/test/java/com/dbserver/coopvote/sessaoVotacao/application/service/SessaoVotacaoAplicationServiceTest.java
@@ -1,13 +1,18 @@
 package test.java.com.dbserver.coopvote.sessaoVotacao.application.service;
 
+import com.dbserver.coopvote.associado.application.service.AssociadoService;
 import com.dbserver.coopvote.pauta.application.controller.PautaNovaRequest;
 import com.dbserver.coopvote.pauta.application.repository.PautaRepository;
 import com.dbserver.coopvote.pauta.domain.Pauta;
 import com.dbserver.coopvote.sessaoVotacao.application.controller.SessaoAbertaResponse;
 import com.dbserver.coopvote.sessaoVotacao.application.controller.SessaoAberturaResquest;
+import com.dbserver.coopvote.sessaoVotacao.application.controller.VotoRequest;
+import com.dbserver.coopvote.sessaoVotacao.application.controller.VotoResponse;
 import com.dbserver.coopvote.sessaoVotacao.application.repository.SessaoVotacaoRepository;
 import com.dbserver.coopvote.sessaoVotacao.application.service.SessaoVotacaoAplicationService;
+import com.dbserver.coopvote.sessaoVotacao.domain.OpcaoVoto;
 import com.dbserver.coopvote.sessaoVotacao.domain.SessaoVotacao;
+import com.dbserver.coopvote.sessaoVotacao.domain.Voto;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.Test;
@@ -18,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -32,6 +38,8 @@ class SessaoVotacaoAplicationServiceTest {
     private SessaoVotacaoRepository sessaoVotacaoRepository;
     @Mock
     private PautaRepository pautaRepository;
+    @Mock
+    private AssociadoService associadoService;
 
     @Test
     public void deveUsarDuracaoPadraoDeUmMinutoQuandoDuracaoNaoForInformadaAoAbrirSessao(){
@@ -69,5 +77,73 @@ class SessaoVotacaoAplicationServiceTest {
 
         verify(sessaoVotacaoRepository, times(0)).save(any(SessaoVotacao.class));
         assertEquals("Pauta não encontrada com id: " + idPauta,exception.getMessage());
+    }
+
+    @Test
+    public void deveRegistrarVotoQuandoCPFAssociadoEValido(){
+        UUID idAssociadoCriador = UUID.randomUUID();
+        PautaNovaRequest request = PautaNovaRequest.builder()
+                .titulo("Aprovação do Novo Plano de Benefícios")
+                .descricao("Será discutida a aprovação do novo plano de benefícios para os associados.")
+                .idAssociadoCriador(idAssociadoCriador)
+                .build();
+        Pauta pauta = new Pauta(request);
+
+        SessaoAberturaResquest sessaoRequest = SessaoAberturaResquest.builder()
+                .idPauta(pauta.getId())
+                .build();
+        SessaoVotacao sessao = new SessaoVotacao(sessaoRequest, pauta);
+        String cpfValido = "63017285995";
+        VotoRequest votoRequest = VotoRequest.builder()
+                .cpfAssociado(cpfValido)
+                .opcaoVoto(OpcaoVoto.SIM)
+                .build();
+        Voto voto = new Voto(sessao, votoRequest);
+        VotoResponse responseExpected = new VotoResponse(voto);
+
+        when(sessaoVotacaoRepository.buscaSessaoPorId(sessao.getId())).thenReturn(sessao);
+        doNothing().when(sessaoVotacaoRepository).save(sessao);
+
+        VotoResponse responseTest = sessaoVotacaoAplicationService.registraVoto(sessao.getId(), votoRequest);
+
+        verify(sessaoVotacaoRepository, times(1)).save(sessao);
+        verify(sessaoVotacaoRepository, times(1)).buscaSessaoPorId(sessao.getId());
+        assertThat(responseExpected.getCpfAssociado()).isEqualTo(responseTest.getCpfAssociado());
+        assertThat(responseExpected.getOpcaoVoto()).isEqualTo(responseTest.getOpcaoVoto());
+    }
+
+    @Test
+    public void naoDeveRegistrarVotoQuandoCPFAssociadoEInvalido(){
+        UUID idAssociadoCriador = UUID.randomUUID();
+        PautaNovaRequest request = PautaNovaRequest.builder()
+                .titulo("Aprovação do Novo Plano de Benefícios")
+                .descricao("Será discutida a aprovação do novo plano de benefícios para os associados.")
+                .idAssociadoCriador(idAssociadoCriador)
+                .build();
+        Pauta pauta = new Pauta(request);
+
+        SessaoAberturaResquest sessaoRequest = SessaoAberturaResquest.builder()
+                .idPauta(pauta.getId())
+                .build();
+        SessaoVotacao sessao = new SessaoVotacao(sessaoRequest, pauta);
+        String cpfInvalido = "40532176871";
+        VotoRequest votoRequest = VotoRequest.builder()
+                .cpfAssociado(cpfInvalido)
+                .opcaoVoto(OpcaoVoto.SIM)
+                .build();
+        Voto voto = new Voto(sessao, votoRequest);
+
+        when(sessaoVotacaoRepository.buscaSessaoPorId(sessao.getId())).thenReturn(sessao);
+
+        doThrow(new RuntimeException("CPF Não é válido para votação!"))
+                .when(associadoService).validaAptidaoAoVoto(voto.getCpfAssociado());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            sessaoVotacaoAplicationService.registraVoto(sessao.getId(), votoRequest);
+        });
+
+        verify(sessaoVotacaoRepository, times(0)).save(sessao);
+        verify(sessaoVotacaoRepository, times(1)).buscaSessaoPorId(sessao.getId());
+        assertThat(exception.getMessage()).isEqualTo("CPF Não é válido para votação!");
     }
 }
